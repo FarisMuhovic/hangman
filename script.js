58b623fea1f571ec25c497d5eac6443b838bb3d5
FarisMuhovic <faris.fare123456@gmail.com>
2022-11-02 23:14:52 +0100

    functional

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// enter a word or let the computer generate a random word //
-// guess it :D !
 const alphabet = [
   "A",
   "B",
@@ -33,17 +31,48 @@ alphabet.forEach(letter => {
   keyboardDiv.innerHTML += `<div data-key=${letter.toLowerCase()}>${letter}</div>`;
 });
 const guessingDisplay = document.getElementById("guessWord");
-word = "faris";
-let wordArray = [];
-for (let i = 0; i < word.length; i++) {
-  wordArray.push(word[i]);
-  guessingDisplay.innerHTML += `<p data-place=${i}></p>`;
-}
+const inputField = document.getElementById("input-word");
+const inputBtn = document.getElementById("inputWordBtn");
+const menu = document.getElementById("menu");
+const curtainbg = document.getElementById("bgcurtain");
+inputBtn.addEventListener("click", () => {
+  word = inputField.value;
+  let wordArray = [];
+  for (let i = 0; i < word.length; i++) {
+    wordArray.push(word[i]);
+    guessingDisplay.innerHTML += `<p data-place=${i}></p>`;
+  }
+  menu.style.display = "none";
+  curtainbg.style.display = "none";
+});
 const keys = document.querySelectorAll("#keyboard div");
 const figure = document.querySelector("#figure");
+let fails = 0;
+//let guessStr = String();
 keys.forEach(key => {
-  key.addEventListener("click", () => {
+  key.addEventListener("click", function QuandaleDingle() {
     let dataKey = key.getAttribute("data-key");
+    word = inputField.value;
+    let wordArray = [];
+    for (let i = 0; i < word.length; i++) {
+      wordArray.push(word[i]);
+    }
+    if (!word.includes(dataKey)) {
+      key.removeEventListener("click", QuandaleDingle);
+      key.style.backgroundColor = "#3360676c";
+      key.style.color = "rgba(255, 255, 255, 0.338)";
+      fails++;
+      let lastchildren = figure.lastElementChild;
+      lastchildren.remove();
+      if (fails == 6) {
+        alert("Game Over, you lost! The word was " + word);
+        if (confirm) {
+          location.reload();
+        } else {
+          location.reload();
+        }
+      }
+    }
     wordArray.forEach(letter => {
       if (letter == dataKey) {
         const indexes = [];
@@ -59,14 +88,24 @@ keys.forEach(key => {
               document.querySelector(
                 `[data-place=${CSS.escape(index)}]`
               ).innerHTML = letter;
+              //  console.log(letter);
+              //guessStr += letter;
             }
           });
         });
-      } //else {
-      //figure.removeChild(figure.lastChild);
-      // key.ariaDisabled;
-      // key.style.backgroundColor = "red";
-      //}
+      }
     });
+    //if (word == guessStr) {
+    //      alert(
+    //        "You won! The word was " + word + " You made " + fails + " mistakes"
+    //     );
+    //     if (confirm) {
+    //       location.reload();
+    //     } else {
+    //       location.reload();
+    //     }
+    //    }
   });
 });
+// if correct guesses = word lenght you won !
+//
